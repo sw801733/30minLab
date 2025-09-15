8fd7c4be67a0058737b6329089bd2c0070696604
sw801733 <sw801733@naver.com>
2025-09-15 22:11:52 +0900

    Rust 실험 : 컴파일과 실행

---
+++ code/hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4.d
@@ -0,0 +1,5 @@
+/Users/sunwoo/Desktop/내 프로젝트/30minLab/code/hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4.d: src/main.rs
+
+/Users/sunwoo/Desktop/내 프로젝트/30minLab/code/hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4: src/main.rs
+
+src/main.rs:
---
+++ code/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/sunwoo/Desktop/내\ 프로젝트/30minLab/code/hello_cargo/target/debug/hello_cargo: /Users/sunwoo/Desktop/내\ 프로젝트/30minLab/code/hello_cargo/src/main.rs
